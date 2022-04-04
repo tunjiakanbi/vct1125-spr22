99495cba4eac2edf35fa6fcd734f0b5572c0b730
tunjiakanbi <tunjiakanbi@gmail.com>
2022-04-04 13:03:31 -0500

    updates

---
+++ vct1125-week13/js/script.js
@@ -0,0 +1,6 @@
+
+// let $box1 = document.getElementById("box1");
+// console.log($box1);
+
+// setInterval(function () {$box1.classList.add('box1')}, 2000);
+// setInterval(function () {$box1.classList.remove('box1')}, 4000);
